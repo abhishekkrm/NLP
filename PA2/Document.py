431a715a648db6fc8510fe262fa9ebf90f21bd69
guillaume <a@a.com>
2015-04-14 10:04:12 -0400

    Added parsing of questions and relevant documents

--- PA2/Document.py
+++ PA2/Document.py
@@ -3,8 +3,13 @@
 ''' A class representing a document returned by IR system
 '''
 class Document(object):
-    def __init__(self, document_file):
-        self.__ParseDocumentFile(document_file)
+    def __init__(self, document_data):
+        self.__ParseDocument(document_data)
         
-    def __ParseDocumentFile(self, document_file):
-        pass
+    def __ParseDocument(self, document_data):
+        self.score=float(document_data.split("Score: ")[-1].split("\n")[0])
+        self.text=document_data.split("<TEXT>\n")[-1].split("\n</TEXT>")[0]
+        # Remove <P> and </P> and \n
+        self.text=self.text.replace("<P>\n","").replace("</P>\n","").replace("\n"," ").replace("  "," ")
+        self.text=self.text.replace("<P>","").replace("</P>","")
+      	
--- PA2/main.py
+++ PA2/main.py
@@ -1,15 +1,17 @@
 import os
+from Question import *
+from Document import *
 
 this_file_path = os.path.realpath(os.path.basename(__file__))
 
-answers_patterns_file = os.path.join(this_file_path, 'pa2-release', 'qadata', 'dev', 'answer_patterns.txt')
-relevent_docs_file = os.path.join(this_file_path, 'pa2-release', 'qadata', 'dev', 'relevant_docs.txt')
+answers_patterns_file = os.path.join('pa2-release', 'qadata', 'dev', 'answer_patterns.txt')
+relevent_docs_file = os.path.join('pa2-release', 'qadata', 'dev', 'relevant_docs.txt')
 
-dev_questions_file = os.path.join(this_file_path, 'pa2-release', 'qadata', 'dev','questions.txt')
-test_questions_file = os.path.join(this_file_path, 'pa2-release', 'qadata', 'test','questions.txt')
+dev_questions_file = os.path.join('pa2-release', 'qadata', 'dev','questions.txt')
+test_questions_file = os.path.join('pa2-release', 'qadata', 'test','questions.txt')
 
-dev_top_docs_folder = os.path.join(this_file_path, 'pa2-release', 'topdocs', 'dev')
-test_top_docs_folder = os.path.join(this_file_path, 'pa2-release', 'topdocs', 'test')
+dev_top_docs_folder = os.path.join('pa2-release', 'topdocs', 'dev')
+test_top_docs_folder = os.path.join('pa2-release', 'topdocs', 'test')
 
 
 class Controller(object):
@@ -21,13 +23,33 @@ class Controller(object):
     ''' Read question number and raw question from file and create question object
     '''
     def __ParseQuestionsFile(self, questions_file):
-        pass
+    	i=0
+        questionNumber=0
+        with open(questions_file, "r") as ins:
+            for line in ins:
+	        if i%3==0:
+	            questionNumber=int(line.rstrip("\r\n").lstrip("Number: "))	
+	        if i%3==1:
+                    self.__questions[questionNumber]=Question(questionNumber,line.rstrip("\r\n"))
+                i+=1
     
     ''' Parses top documents and creates a list of Document objects for each Question Object 
     '''
     def __ParseTopDocuments(self, top_docs_folder):
-        pass
-    
+        for questionNo in self.__questions:
+            top_docs_file= os.path.join(top_docs_folder,"top_docs."+str(questionNo))
+            text=""
+            with open(top_docs_file, "r") as ins:
+	        for line in ins:
+		    text+=line
+            splitDocuments=text.split("Qid: ") 
+            del splitDocuments[0]
+            top_documents=[]
+            for textDocument in splitDocuments:
+                document=Document(textDocument)
+                top_documents.append(document)                               
+            self.__questions[questionNo].SetTopDocuments(top_documents)
+                
     def __ProcessQuesion(self, question, question_processor):
         pass
     
