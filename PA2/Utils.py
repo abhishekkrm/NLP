e958a7cb2f672ef6e1832d527230f952c0605f9a
Gaurav <ga286@cornell.edu>
2015-04-18 18:45:50 -0400

    Adding some more utilities.
    Signed-off-by: Gaurav <ga286@cornell.edu>

--- PA2/Utils.py
+++ PA2/Utils.py
@@ -1,10 +1,18 @@
 import os
+import nltk
 from nltk.corpus import stopwords
 from nltk.tag.stanford import NERTagger
+from nltk.util import ngrams
 
 #constants
 this_file_path = os.path.dirname(os.path.realpath(os.path.basename(__file__)))
 stanford_ner_folder = os.path.join(this_file_path, 'stanford-ner-2015-01-30')
+
+#Creating global - For optimization:
+ner_jar_path = os.path.join(stanford_ner_folder, 'stanford-ner.jar')
+ner_classifier_path = os.path.join(stanford_ner_folder, 'classifiers', 'english.all.7class.distsim.crf.ser.gz') 
+ner_tagger = NERTagger(ner_classifier_path, ner_jar_path)
+    
 
 ''' Remove the stopwords in the given text and return new string
 '''
@@ -16,13 +24,21 @@ def RemoveStopwords(text):
 ''' Given a list of words returns a list of tuples of type (word, NE tag)
 '''
 def TagNamedEntitiesInList(word_list):
-    ner_jar = os.path.join(stanford_ner_folder, 'stanford-ner.jar')
-    ner_classifier = os.path.join(stanford_ner_folder, 'classifiers', 'english.all.7class.distsim.crf.ser.gz') 
-    
-    tagger = NERTagger(ner_classifier, ner_jar)
-    return tagger.tag(word_list)
+    return ner_tagger.tag(word_list)
 
 ''' Given a text returns a list of tuples of type (word, NE tag)
 '''
 def TagNamedEntities(text):
     return TagNamedEntitiesInList(text.split())
+
+''' Given a text returns a list of tuples of type (word, POS tag)
+'''
+def POSTag(text):
+    tokens = text.split()
+    return nltk.pos_tag(tokens)
+
+''' Given a text returns a list of ngrams
+'''
+def GetNGrams(text, N = 2):
+    return ngrams(text.split(), N)
+
