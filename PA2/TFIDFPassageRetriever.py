f96253162682e3a2c1ea5c5f372885a8b1dbb6d1
Jai Bhatt <jdb424@cornell.edu>
2015-04-20 14:35:06 -0400

    TF IDF passage retriever implementation using Synsets

--- PA2/TFIDFPassageRetriever.py
+++ PA2/TFIDFPassageRetriever.py
@@ -2,6 +2,9 @@ import Utils
 import operator
 from PassageRetriever import IPassageRetriever
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import nltk
+from nltk.corpus import wordnet as wn
 
 class TFIDFPassageRetriever(IPassageRetriever):
     
@@ -33,18 +36,52 @@ class TFIDFPassageRetriever(IPassageRetriever):
     '''
     def GetRelatedPassages(self, question, n=10):
         # Gather all the sentences of all the documents (top documents for the given question) and their similarity score
-        sentenceRelevance={}
         
+        question_text = self.GetExpandedQuestion(question)
+        print("Expanded question : "+question_text)
+        sentenceRelevance={}
+        corpus = []
+        corpus.append(question_text)
         topDocuments=question.GetTopDocuments()
         for document in topDocuments:
-            sentenceRelevance.update(self.__GetTFIDFSentenceRelevance(document, question))
+            #sentenceRelevance.update(self.__GetTFIDFSentenceRelevance(document, question))
+            for sentence in document.GetPassages():
+                corpus.append(sentence)
+            #corpus.append(document.GetText())
+        print("Number of sentences in corpus : "+str(len(corpus)))
+        tfidf_vectorizer = TfidfVectorizer()
+        tfidf_matrix_train = tfidf_vectorizer.fit_transform(corpus)
+        #print(tfidf_matrix_train)
+        
+        similarity_scores = cosine_similarity(tfidf_matrix_train[0:1], tfidf_matrix_train)
+        #print ("cosine scores ==> "+str(similarity_scores))
+        i =0; 
+        print("score length : "+str(len(similarity_scores[0])))
+        for score in similarity_scores[0]:
+            sentenceRelevance[corpus[i]] = score
+            i=i+1
             
         # We sort the sentences by relevance
         sortedSentenceRelevance = sorted(sentenceRelevance.items(), key=operator.itemgetter(1), reverse=True)
         
         relatedPassages=[]
         for i in range(n):
-            relatedPassages.append(sortedSentenceRelevance[i][0])
+            relatedPassages.append(sortedSentenceRelevance[i+1][0])
         
         return relatedPassages
 
+    def GetExpandedQuestion(self,question):
+        tokens = nltk.word_tokenize(question.GetRawQuestion())
+        new_tokens = []
+        for token in tokens:
+            #print("Token : "+token)
+            if len(wn.synsets(token)) > 0:
+                for syn in wn.synsets(token):
+                    for lemma in syn.lemmas():
+                        #print (lemma.name())
+                        if(lemma.name().replace("_"," ") not in new_tokens):
+                            new_tokens.append(lemma.name().replace("_"," "))
+            else:
+                if(token not in new_tokens):
+                    new_tokens.append(token)
+        return " ".join(new_tokens)
